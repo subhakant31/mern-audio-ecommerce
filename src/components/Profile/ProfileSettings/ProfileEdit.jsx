3f446e95656d148a8a16324d9d97c13d9b1ee1cd
sushma <sushmalanke31@gmail.com>
2023-11-07 20:02:38 +0530

    bug fix

--- src/components/Profile/ProfileSettings/ProfileEdit.jsx
+++ src/components/Profile/ProfileSettings/ProfileEdit.jsx
@@ -7,36 +7,36 @@ function ProfileEdit() {
       <div className="settings">
         <div className="settings__column">
           <h3 className="settings__column__title">general</h3>
-          <ul>
-            <li className="list-item">
+          <ul className="list">
+            <li className="list__item">
               <a href="">edit profile</a>
             </li>
-            <li className="list-item">
+            <li className="list__item">
               <a href="">notifications</a>
             </li>
-            <li className="list-item">
+            <li className="list__item">
               <a href="">wishlist</a>
             </li>
           </ul>
         </div>
         <div className="settings__column">
           <h3 className="settings__column__title">legal</h3>
-          <ul>
-            <li className="list-item">
+          <ul  className="list">
+            <li className="list__item">
               <a href="">terms of use</a>
             </li>
-            <li className="list-item">
+            <li className="list__item">
               <a href="">privacy policy</a>
             </li>
           </ul>
         </div>
         <div className="settings__column">
           <h3 className="settings__column__title">personal</h3>
-          <ul>
-            <li className="list-item">
+          <ul  className="list">
+            <li className="list__item">
               <a href="">report a bug</a>
             </li>
-            <li className="list-item">
+            <li className="list__item">
               <a href="">logout</a>
             </li>
           </ul>
