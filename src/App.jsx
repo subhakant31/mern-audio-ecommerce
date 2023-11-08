2a1df3474b883d3fa6fb18e758d8f335d84c636b
subhakanta.mishra <subhakanta.mishra@tadigital.com>
2023-11-08 11:51:33 +0530

    removed routes as of now

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import ProfileSetting from "./components/Profile/ProfileEdit";
 import Homemenubar from "./components/Menu/HomeMenu";
-import "./App.scss"; 
+import "./App.scss";
 import { Route, Routes } from "react-router-dom";
-
-function App(props)
-{
-	return(
-		<React.Fragment>
-			<Routes>
-        <Route path="/home" element={<Homemenubar/>}></Route>
-        <Route path="/profile" element={<ProfileSetting/>}></Route>
-      </Routes>
-		</React.Fragment>
-	)
+import ProductDescriptionPage from "./pages/ProductDescriptionPage/ProductDescriptionPage";
+function App(props) {
+  return (
+    <React.Fragment>
+      <ProductDescriptionPage></ProductDescriptionPage>
+    </React.Fragment>
+  );
 }
 
 export default App;
