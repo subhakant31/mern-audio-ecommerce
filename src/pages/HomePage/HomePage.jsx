68deb93097785feded6b793063ea8593154147fd
subhakanta.mishra <subhakanta.mishra@tadigital.com>
2023-11-09 18:34:24 +0530

    now using the more products component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,41 +1,24 @@
 import "./HomePage.scss";
-import HomeMenu from '../../components/Menu/HomeMenu';
-import SearchComponent from '../../components/SearchComponent/SearchComponent';
-import ProductCategory from '../../components/ProductCategory/ProductCategory'
-import SuggestedProductCard from '../../components/ProductDetails/ProductOverview/SuggestedProductCard/SuggestedProductCard';
+import HomeMenu from "../../components/Menu/HomeMenu";
+import SearchComponent from "../../components/SearchComponent/SearchComponent";
+import ProductCategory from "../../components/ProductCategory/ProductCategory";
+import MoreProducts from "../../components/MoreProducts/MoreProducts";
 
-import '../../components/ProductDetails/ProductOverview/ProductOverview.scss';
+export default function HomePage() {
+  return (
+    <section className="home-page">
+      <HomeMenu />
 
-export default function HomePage () {
-    return (
-        <section className="home-page">            
-	        <HomeMenu />
-       
-            <div className="hero-text">
-                <h2>Hi, Andrea</h2>
-                <span>What are you looking for today?</span>
-            </div>
-            <SearchComponent />
-            <main className='home-page__main'>
-                <ProductCategory/>
+      <div className="hero-text">
+        <h2>Hi, Andrea</h2>
+        <span>What are you looking for today?</span>
+      </div>
+      <SearchComponent />
+      <main className="home-page__main">
+        <ProductCategory />
 
-                <div className="product-overview__more-products">
-                  <div className="product-overview__more-products__heading-container">
-                        <h3 className="heading">another product</h3>
-                        <a href="#">see all</a>
-                   </div>
-                    <ul className="product-overview__more-products__more-product-list">
-                        <SuggestedProductCard></SuggestedProductCard>
-                        <SuggestedProductCard></SuggestedProductCard>
-                        <SuggestedProductCard></SuggestedProductCard>
-                        <SuggestedProductCard></SuggestedProductCard>
-                    </ul>
-                </div>
-            
-            </main>
-
-
-
-        </section>
-    )
+        <MoreProducts heading="featured products"></MoreProducts>
+      </main>
+    </section>
+  );
 }
