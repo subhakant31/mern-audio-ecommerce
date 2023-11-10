2833f0b6f412d6b30329cddb7cd8b3cdd45ec7b0
sushma <sushmalanke31@gmail.com>
2023-11-10 13:41:24 +0530

    bug chnages

--- src/App.jsx
+++ src/App.jsx
@@ -20,31 +20,5 @@ function App(props) {
       <WishlistPage></WishlistPage>
     </React.Fragment>
   );
-import React, { useState } from "react";
-import SignInPage from "./pages/SignInPage/SignInPage";
-import SignUpPage from "./pages/SignUpPage/SignUpPage";
-import Homemenubar from "./components/Menu/HomeMenu";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import CartPage from "./pages/AddToCartPage/AddToCartPage";
-import WishlistPage from "./pages/WishList/WishlistPage";
-import "./App.scss";
-import FilterPage from "./components/Filter/FilterPage/FilterPage";
-
-function App(props) {
-  // IMPORTANT: This code is responsible for filter popup
-  // Dont remove  just comment out this
-  const [open, setOpen] = useState(false);
-
-  // Function to open and close the popup
-  function show(ev) {
-    setOpen(!open);
-  }
-
-  return (
-    <React.Fragment>
-      <button onClick={show}>Show filter</button>
-      <FilterPage open={open} setOpen={setOpen} />
-    </React.Fragment>
-  );
 }
 export default App;
--- src/pages/AddToCartPage/AddToCartPage.jsx
+++ src/pages/AddToCartPage/AddToCartPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./AddToCartPage.scss";
-import CartProceed from "../../components/CheckOut/CheckOut";
+import CartTotalCost from "../../components/CartTotalCost/CartTotalCost";
+import CheckoutCart from '../../components/ProductActionButtonWithArrow/ProductActionButtonWithArrow';
 import ProductItem from "../../components/ShoppingProduct/ProductItem/ProductItem";
 function AddToCartPage() {
   return (
     <div className="cartcontainer">
       <ProductItem></ProductItem>
-      <CartProceed></CartProceed>
+      <CartTotalCost></CartTotalCost>
+      <CheckoutCart
+        title="checkout button"
+        ariaLabelText="add this item to cart"
+        buttonText="proceed to checkout"
+      />
     </div>
   );
 }
