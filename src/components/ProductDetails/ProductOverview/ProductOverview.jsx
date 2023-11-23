c051bbd37532dd7f12b16ec5db7b9d54282e9ac6
subhakant31 <subhakanta.mishra3105@gmail.com>
2023-11-23 15:23:13 +0530

    wish list item button added

--- src/components/ProductDetails/ProductOverview/ProductOverview.jsx
+++ src/components/ProductDetails/ProductOverview/ProductOverview.jsx
@@ -3,11 +3,18 @@ import ProductImage from "../ProductImage/ProductImage";
 import ReviewCard from "./ReviewCard/ReviewCard";
 import MoreProducts from "../../MoreProducts/MoreProducts";
 import SuggestedProductCard from "./SuggestedProductCard/SuggestedProductCard";
+import WishListBtn from "../../WishListBtn/WishListBtn";
+import { useState } from "react";
 function ProductOverview(props) {
   const productId = props.id;
+  const { wishListState, setWishListState } = props; //flag of whether is item is wishlisted from parent component
   return (
-    <div className="product-overview" id={productId}>
-      <ul className="product-overview__images-container">
+    <div className='product-overview' id={productId}>
+      <ul className='product-overview__images-container'>
+        <WishListBtn
+          wishListState={wishListState}
+          setWishListState={setWishListState}
+        />
         <ProductImage
           image={`./assets/images/productImages/productimage1.png`}
         ></ProductImage>
@@ -18,24 +25,24 @@ function ProductOverview(props) {
           image={`./assets/images/productImages/productimage1.png`}
         ></ProductImage>
       </ul>
-      <div className="product-overview__user-reviews">
-        <h3 className="product-overview__user-reviews__heading">
+      <div className='product-overview__user-reviews'>
+        <h3 className='product-overview__user-reviews__heading'>
           review (102)
         </h3>
-        <ul className="product-overview__user-reviews__container">
+        <ul className='product-overview__user-reviews__container'>
           <ReviewCard></ReviewCard>
           <ReviewCard></ReviewCard>
           <ReviewCard></ReviewCard>
         </ul>
         <a
-          href="/"
-          aria-label="show more reviews"
-          className="product-overview__user-reviews__see-more-reviews-btn"
+          href='/'
+          aria-label='show more reviews'
+          className='product-overview__user-reviews__see-more-reviews-btn'
         >
           See All Reviews
         </a>
       </div>
-      <MoreProducts heading="more products"></MoreProducts>
+      <MoreProducts heading='more products'></MoreProducts>
     </div>
   );
 }
