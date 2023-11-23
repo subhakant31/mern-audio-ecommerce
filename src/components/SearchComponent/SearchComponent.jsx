60cf65c959bda4486cb8c20ab771f9df9d7ea385
shivalaya yadav <shivalaya.yadav@tadigital.com>
2023-11-23 17:24:05 +0530

    feature/routing: Did routing for search component

--- src/components/SearchComponent/SearchComponent.jsx
+++ src/components/SearchComponent/SearchComponent.jsx
@@ -2,10 +2,18 @@ import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './SearchComponent.scss';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router';
 
 
-export default function SearchComponent() {
+export default function SearchComponent({searchPageRedirect}) {
+    const navigate = useNavigate();
     const [searchValue, setSearchValue] = useState('');
+
+    function navigateToSearchPageHandler() {
+        if(searchPageRedirect===true) {
+            navigate('/searchPage')
+        }
+    }
 
     function inputHandler(event) {
         setSearchValue(() => event.target.value);
@@ -21,7 +29,7 @@ export default function SearchComponent() {
         <form className="search-bar" onSubmit={handleSearch}>
             <label htmlFor="searchInput" className="visually-hidden">Search for Products</label>
             <FontAwesomeIcon className="search-bar__icon" aria-hidden="true" icon={faSearch} />
-            <input type="text" id="searchInput" value={searchValue} onChange={inputHandler} placeholder="Search headphone" aria-label='search' aria-autocomplete='list'/>
+            <input type="text" id="searchInput" value={searchValue} onChange={inputHandler} onFocus={navigateToSearchPageHandler} placeholder="Search headphone" aria-label='search' aria-autocomplete='list'/>
         </form>
     )
 }
