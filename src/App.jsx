d142a3be45ec6beb8521b0637cfbaadca248272d
subhakant31 <subhakanta.mishra3105@gmail.com>
2023-11-23 15:28:42 +0530

    added product description page

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,25 @@ import WishlistPage from "./pages/WishList/WishlistPage";
 import AddToCartPage from "./pages/AddToCartPage/AddToCartPage";
 import SearchProductPage from "./pages/SearchProductPage/SearchProductPage";
 import ResultsPage from "./pages/ResultsPage/ResultsPage";
-import ProductDescriptionPage from "./pages/ProductDescriptionPage/ProductDescriptionPage";import ProductDescriptionPage from "./pages/ProductDescriptionPage/ProductDescriptionPage";
+import ProductDescriptionPage from "./pages/ProductDescriptionPage/ProductDescriptionPage";
 
 function App() {
   return (
     <React.Fragment>
       <Routes>
-        <Route path="/" element={<SignInPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/results" element={<ResultsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="privacyPolicy" element={<PrivacyPolicy />} />
-        <Route path="termsofuse" element={<TermsOfUse />} />
-        <Route path="wishlist" element={<WishlistPage />} />
-        <Route path="/productdetails" element={<ProductDescriptionPage />} />
-        <Route path="/cart" element={<AddToCartPage />} />
-        <Route path="/searchPage" element={<SearchProductPage />} />
+        <Route path='/' element={<SignInPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/results' element={<ResultsPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='privacyPolicy' element={<PrivacyPolicy />} />
+        <Route path='termsofuse' element={<TermsOfUse />} />
+        <Route path='wishlist' element={<WishlistPage />} />
+        <Route path='/productdetails' element={<ProductDescriptionPage />} />
+        <Route path='/cart' element={<AddToCartPage />} />
+        <Route path='/searchPage' element={<SearchProductPage />} />
       </Routes>
     </React.Fragment>
   );
-;
 }
 export default App;
